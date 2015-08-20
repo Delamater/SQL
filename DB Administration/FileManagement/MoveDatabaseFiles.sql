98fb394871c6242058ad2a838eeb92180208b863
Bob Delamater <bob.delamater@sage.com>
2015-08-20 06:40:17 -0700

    Update MoveDatabaseFiles.sql

--- DB Administration/FileManagement/MoveDatabaseFiles.sql
+++ DB Administration/FileManagement/MoveDatabaseFiles.sql
@@ -1,10 +1,14 @@
+-- Author: Bob Delamater			
+-- Description: Informs SQL Server that you are moving the log and data files to a new location.
+-- 		If a file exists in the destination path, it will be overwritten when you restart SQL Server.
+--		For this reason, be absolutely careful you do not overwrite a production database file.
 --Move database and log files to E drive
 DECLARE @dbName SYSNAME, 
 		@newLocDataFilePath	VARCHAR(255),
 		@newLocLogFilePath	VARCHAR(255),
 
-SET @newLocDataFilePath = 'E:\Elmer\Data\x3v6_data.mdf'
-SET @newLocLogFilePath = 'E:\Elmer\Data\x3v6_log.ldf'
+SET @newLocDataFilePath = 'E:\MyFolder\Data\x3v6_data.mdf'
+SET @newLocLogFilePath = 'E:\MyFolder\Data\x3v6_log.ldf'
 
 USE master
 SELECT name, physical_name
@@ -15,9 +19,9 @@ ALTER DATABASE x3v6
 SET OFFLINE
 
 ALTER DATABASE  x3v6
-MODIFY FILE( Name = x3v6_data, FILENAME = 'E:\Elmer\Data\x3v6_data.mdf')
+MODIFY FILE( Name = x3v6_data, FILENAME = 'E:\MyFolder\Data\x3v6_data.mdf')
 ALTER DATABASE  x3v6
-MODIFY FILE( Name = x3v6_log, FILENAME = 'E:\Elmer\Data\x3v6_log.ldf')
+MODIFY FILE( Name = x3v6_log, FILENAME = 'E:\MyFolder\Data\x3v6_log.ldf')
 
 ALTER DATABASE x3v6
 SET ONLINE
