b50761d47f76a0abe5e9c771590777d7c2b226d5
Bob Delamater <bob.delamater@sage.com>
2016-02-17 08:26:31 -0800

    Update GetTruncateTableUser.sql

--- Log File/GetTruncateTableUser.sql
+++ Log File/GetTruncateTableUser.sql
@@ -1,4 +1,5 @@
 -- Locate Truncate Table User
-SELECT [Transaction Name], SUSER_SNAME([Transaction SID]) As UserName, [Begin Time], Operation, [Transaction ID], Description 
+SELECT AllocUnitName, [Transaction Name], SUSER_SNAME([Transaction SID]) As UserName, [Begin Time], Operation, [Transaction ID], Description, *
 FROM fn_dblog(null, null) 
-WHERE LOWER([Transaction Name]) like LOWER('%truncate%')
+WHERE LOWER([Transaction Name]) LIKE LOWER('%truncate%') 
+	OR LOWER([Transaction Name]) LIKE LOWER('%delete%')
