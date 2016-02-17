e9704401c12fb9aaeff2104078c1ac07bf3505cd
Bob Delamater <bob.delamater@sage.com>
2016-02-17 08:12:17 -0800

    Changed transaction name to be case insensitive search

--- Log File/GetTruncateTableUser.sql
+++ Log File/GetTruncateTableUser.sql
@@ -1,4 +1,4 @@
 -- Locate Truncate Table User
 SELECT [Transaction Name], SUSER_SNAME([Transaction SID]) As UserName, [Begin Time], Operation, [Transaction ID], Description 
 FROM fn_dblog(null, null) 
-WHERE [Transaction Name] like 'truncate%'
+WHERE LOWER([Transaction Name]) like LOWER('%truncate%')
