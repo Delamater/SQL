afc4fb06a4ca2892ae70fdc8884713c53d1ce87f
Bob Delamater <bob.delamater@sage.com>
2016-08-10 14:57:31 -0700

    Create DiskSpd.ps1

---
+++ Performance/SAN/DiskSpd.ps1
@@ -0,0 +1,3 @@
+E:\Diskspd-v2.0.17\amd64fre\diskspd.exe -b8K -d10 -Sh -L -o8 -r -w20 -eDISK_IO -c1G e:\Diskspd-v2.0.17\RunResults\io.dat > e:\Diskspd-v2.0.17\RunResults\Results1.txt
+E:\Diskspd-v2.0.17\amd64fre\diskspd.exe -b8K -d10 -Sh -L -o8 -r -w20 -eDISK_IO  -c10G e:\Diskspd-v2.0.17\RunResults\io.dat > e:\Diskspd-v2.0.17\RunResults\Results2.txt
+E:\Diskspd-v2.0.17\amd64fre\diskspd.exe -b8K -d600 -Sh -L -o8 -r -w20 -eDISK_IO  -c20G e:\Diskspd-v2.0.17\RunResults\io.dat > e:\Diskspd-v2.0.17\RunResults\Results3.txt
