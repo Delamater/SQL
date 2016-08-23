5b71a25cbf9cc2f0288e688b6c17e3a7a38ecfa9
Bob Delamater <bob.delamater@sage.com>
2016-08-23 14:42:46 -0700

    Added Row Counts / Removed Filter

--- Index/GetIndexUsageStatistics.sql
+++ Index/GetIndexUsageStatistics.sql
@@ -3,10 +3,9 @@ SELECT
    , DB_NAME() AS [DatabaseName]
    , SCHEMA_NAME([sObj].[schema_id]) AS [SchemaName]
    , [sObj].[name] AS [ObjectName]
-   , CASE
-      WHEN [sObj].[type] = 'U' THEN 'Table'
-      WHEN [sObj].[type] = 'V' THEN 'View'
-      END AS [ObjectType]
+   , [sObj].type_desc [ObjectType]
+   , [sIdx].is_unique IsUnique
+   , [ps].row_count
    , [sIdx].[index_id] AS [IndexID]
    , ISNULL([sIdx].[name], 'N/A') AS [IndexName]
    , [sIdx].type_desc
@@ -26,6 +25,9 @@ FROM
    [sys].[indexes] AS [sIdx]
    INNER JOIN [sys].[objects] AS [sObj]
       ON [sIdx].[object_id] = [sObj].[object_id]
+   LEFT JOIN [sys].[dm_db_partition_stats] ps	-- Stats might not exist
+	  ON [sIdx].object_id = [ps].object_id
+	  AND [sIdx].index_id = ps.index_id
    LEFT JOIN [sys].[dm_db_index_usage_stats] AS [sdmvIUS]
       ON [sIdx].[object_id] = [sdmvIUS].[object_id]
       AND [sIdx].[index_id] = [sdmvIUS].[index_id]
@@ -37,6 +39,4 @@ WHERE
    [sObj].[type] IN ('U','V')         -- Look in Tables & Views
    AND [sObj].[is_ms_shipped] = 0x0   -- Exclude System Generated Objects
    AND [sIdx].[is_disabled] = 0x0     -- Exclude Disabled Indexes
-   AND [sObj].[name] = 'STOJOU'
-
-
+	
