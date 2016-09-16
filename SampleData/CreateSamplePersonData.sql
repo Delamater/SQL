7076b112c3ca0f4dda126cb3b565b7029cd12179
Bob Delamater <bob.delamater@sage.com>
2016-09-15 19:08:56 -0700

    Made the random number columns make more sense

--- SampleData/CreateSamplePersonData.sql
+++ SampleData/CreateSamplePersonData.sql
@@ -1,31 +1,31 @@
+-- DROP TABLE SampleData
 CREATE TABLE SampleData
 (
-	ID				INT	IDENTITY(1,1),
-	SocialSecurity	VARCHAR(256),
-	Notes			VARCHAR(256),
-	StateID			CHAR(2),
-	RandNum1		DECIMAL(18,2),
-	RandNum2		DECIMAL(18,2),
-	ProductNum1Num2 AS RandNum1 * RandNum2 PERSISTED
+	ID					INT	IDENTITY(1,1),
+	SocialSecurity		VARCHAR(256),
+	Notes				VARCHAR(256),
+	StateID				CHAR(2),
+	WageRate			DECIMAL(18,2),
+	WorkHoursPerWeek	DECIMAL(18,2),
+	WeeklyPay AS WageRate * WorkHoursPerWeek PERSISTED
 )
 GO
 WITH
 	Person AS (SELECT CHECKSUM(NEWID()) AS SocialSecurity, NEWID() AS Notes),
 	States AS (SELECT SUBSTRING(CAST(NEWID() AS VARCHAR(100)),0,3) AS StateID)
-INSERT INTO SampleData(SocialSecurity, Notes, StateID, RandNum1, RandNum2)
+INSERT INTO SampleData(SocialSecurity, Notes, StateID, WageRate, WorkHoursPerWeek)
 SELECT TOP 1000000
 	Person.SocialSecurity,
 	Person.Notes,
 	States.StateID,
-	ABS(CHECKSUM(NewId())) % 100,
-	ABS(CHECKSUM(NewId())) % 100
+	ABS(CHECKSUM(NewId())) % 501, -- Pay rate can be anywhere from 0 to 500 per hour
+	ABS(CHECKSUM(NewId())) % 81		-- Worked hours can be anywhere from 0 to 80 hours worked a week
 --INTO #
 FROM Person 
 	CROSS JOIN States 
 	CROSS JOIN sys.columns 
 	CROSS JOIN sys.objects
 	CROSS JOIN sys.tables
-
 
 --sp_spaceused SampleData
 --CREATE UNIQUE NONCLUSTERED INDEX idx1 ON SampleData(ID, SocialSecurity, Notes, StateID)
