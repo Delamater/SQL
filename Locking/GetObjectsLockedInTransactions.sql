32fedecc2f15739446ecad6b43e887fd5356eec0
Bob Delamater <bob.delamater@sage.com>
2017-10-11 08:27:04 -0700

    Update GetObjectsLockedInTransactions.sql

--- Locking/GetObjectsLockedInTransactions.sql
+++ Locking/GetObjectsLockedInTransactions.sql
@@ -1,7 +1,6 @@
---select * from DLVV11.XAPCKASS
---exec beta_lockinfo 0,0,'F',null
-
 SELECT 
+	db_name(dl.resource_database_id) DatabaseName,
+	ao.name ObjectName,
 	es.login_time,
 	es.host_name,
 	es.program_name,
@@ -11,18 +10,13 @@ SELECT
 	es.memory_usage,
 	es.total_scheduled_time,
 	es.total_elapsed_time,
-	ao.name ObjectName,
 	dl.resource_type, 
-	db_name(dl.resource_database_id) DatabaseName,
 	dl.request_mode,
 	dl.request_type,
 	dl.request_status,
 	dl.request_reference_count,
 	dl.request_lifetime,
 	dl.request_session_id
-	--resource_type, resource_associated_entity_id,  
- --   request_status, request_mode,request_session_id,  
- --   resource_description   
 FROM sys.dm_tran_locks  dl with(nolock)
 	LEFT JOIN sys.all_objects ao with(nolock)
 		ON dl.resource_associated_entity_id = ao.object_id
