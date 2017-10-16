045d2bd168c86bf89689ab3fbd45037a089ec408
Bob Delamater <bob.delamater@sage.com>
2017-10-16 09:27:11 -0700

    Parameterized
    Press CTRL+SHIFT+M to replace the parameters while in SSMS

--- SearchByColumnBySchema.sql
+++ SearchByColumnBySchema.sql
@@ -1,10 +1,10 @@
-SELECT s.name Folder, t.name TableName, c.name ColumnName
-FROM sys.tables t
-	INNER JOIN sys.columns c
+SELECT s.name SchemaName, t.name TableName, c.name ColumnName
+FROM sys.columns c
+	INNER JOIN sys.tables t
 		ON t.object_id = c.object_id
 	INNER JOIN sys.schemas s
 		ON t.schema_id = s.schema_id
 WHERE 
-	s.name = 'DEMO' 
-	AND c.name LIKE '%PID%'
-ORDER BY s.name, t.name, c.name
+	c.name = '<Column Name, SYSNAM, CPY_0>'
+	AND s.name = '<Schema Name, SYSNAME, SEED>'
+ORDER BY t.name, c.name
