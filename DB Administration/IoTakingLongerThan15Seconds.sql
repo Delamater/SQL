6e5f1a4b7b8bf3fec97d368c217f35aaf2b4e488
Bob Delamater <bob.delamater@sage.com>
2018-01-08 08:41:43 -0800

    Create IoTakingLongerThan15Seconds.sql

---
+++ DB Administration/IoTakingLongerThan15Seconds.sql
@@ -0,0 +1,32 @@
+
+-- Look for I/O requests taking longer than 15 seconds in the five most recent SQL Server Error Logs (Query 31) (IO Warnings)
+CREATE TABLE #IOWarningResults(LogDate datetime, ProcessInfo sysname, LogText nvarchar(1000));
+
+	INSERT INTO #IOWarningResults 
+	EXEC xp_readerrorlog 0, 1, N'taking longer than 15 seconds';
+
+	INSERT INTO #IOWarningResults 
+	EXEC xp_readerrorlog 1, 1, N'taking longer than 15 seconds';
+
+	INSERT INTO #IOWarningResults 
+	EXEC xp_readerrorlog 2, 1, N'taking longer than 15 seconds';
+
+	INSERT INTO #IOWarningResults 
+	EXEC xp_readerrorlog 3, 1, N'taking longer than 15 seconds';
+
+	INSERT INTO #IOWarningResults 
+	EXEC xp_readerrorlog 4, 1, N'taking longer than 15 seconds';
+
+SELECT LogDate, ProcessInfo, LogText
+FROM #IOWarningResults
+ORDER BY LogDate DESC;
+
+DROP TABLE #IOWarningResults;
+------  
+
+-- Finding 15 second I/O warnings in the SQL Server Error Log is useful evidence of
+-- poor I/O performance (which might have many different causes)
+-- Look to see if you see any patterns in the results (same files, same drives, same time of day, etc.)
+
+-- Diagnostics in SQL Server help detect stalled and stuck I/O operations
+-- https://support.microsoft.com/en-us/kb/897284
