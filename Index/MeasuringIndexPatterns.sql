4c532d7bb16016fc65439660cdb822d4d5a2e19f
Bob Delamater <bob.delamater@sage.com>
2018-01-21 17:18:14 -0800

    Create MeasuringIndexPatterns.sql

---
+++ Index/MeasuringIndexPatterns.sql
@@ -0,0 +1,196 @@
+DBCC SQLPERF (N'sys.dm_os_wait_stats', CLEAR);
+GO
+/****** Declare Variables ******/ 
+DECLARE @InsertionStart DATETIME2, @InsertionEnd DATETIME2
+DECLARE @ReadStart DATETIME2, @ReadEnd DATETIME2
+DECLARE @CurrentPageSplitValue INT
+DECLARE @RandGUIDValue UNIQUEIDENTIFIER
+SET @InsertionStart = CURRENT_TIMESTAMP
+SET @CurrentPageSplitValue = (select cntr_value from sys.dm_os_performance_counters where counter_name = 'Page Splits/sec') 
+
+
+/****** Create Objects ******/ 
+CREATE SEQUENCE ID START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE ClusteredIndexExample
+(
+	ID			INT NOT NULL,
+	UniqueGUID	UNIQUEIDENTIFIER NOT NULL,
+	LargeText	CHAR(8000) NOT NULL
+);
+
+
+/****** Set Index Type ******/ 
+CREATE CLUSTERED INDEX clsGUID ON ClusteredIndexExample(UniqueGUID) --WITH(FILLFACTOR = 10);
+--CREATE NONCLUSTERED INDEX clsGUID ON ClusteredIndexExample(UniqueGUID) --WITH(FILLFACTOR = 10);
+--CREATE NONCLUSTERED INDEX clsGUID ON ClusteredIndexExample(ID) --WITH(FILLFACTOR = 10);
+
+
+/****** Perform Insert ******/ 
+INSERT INTO ClusteredIndexExample(ID,UniqueGUID,LargeText)
+SELECT NEXT VALUE FOR ID, NEWID(), space(8000) 
+FROM sys.columns a, (SELECT TOP 100 name FROM sys.all_objects) b
+
+SET @InsertionEnd = CURRENT_TIMESTAMP
+
+
+SET @ReadStart = CURRENT_TIMESTAMP
+SET @RandGUIDValue = (SELECT UniqueGUID  FROM ClusteredIndexExample WHERE ID = (SELECT MAX(ID) FROM ClusteredIndexExample)/2)
+
+SET STATISTICS IO ON
+SET STATISTICS TIME ON
+SELECT * FROM ClusteredIndexExample WHERE UniqueGUID = @RandGUIDValue
+SET STATISTICS IO OFF
+SET STATISTICS TIME OFF
+SET @ReadEnd = CURRENT_TIMESTAMP
+
+Select 
+	@InsertionStart InsertStart, @InsertionEnd InsertionEnd, DATEDIFF(ss,@InsertionStart,@InsertionEnd) InsertionTimeDelta_Seconds, 
+	@ReadStart ReadStart, @ReadEnd ReadEnd, DATEDIFF(ss,@ReadStart, @ReadEnd) ReadTimeDelta_Seconds,
+	@CurrentPageSplitValue-(select cntr_value from sys.dm_os_performance_counters where counter_name = 'Page Splits/sec') AS PageSplitDelta
+
+SELECT
+	Database_ID,
+	CONVERT(VARCHAR(35),DB_NAME(idx.database_id)) AS [DB Name],
+	sch.name [Schema Name],
+	idx.[Object_ID],CONVERT(VARCHAR(35),OBJECT_NAME(idx.[OBJECT_ID])) AS 'OBJECT NAME',
+	idx.Partition_Number [Partition Number],
+	idx.Index_ID [Index ID],
+	idx.Index_Type_Desc [Index Type Desc],
+	idx.Alloc_Unit_Type_Desc [Alloc Unit Type Desc],
+	idx.Index_Depth,Index_Level [Index Depth Level],
+	idx.Avg_Fragmentation_In_Percent [Avg Frag Percent],
+	idx.Fragment_Count [Frag Count],
+	Avg_Fragment_Size_In_Pages [Avg Frag Size In Pages],
+	idx.Page_Count [Page Count],
+	idx.Avg_Page_Space_Used_In_Percent [Avg Page Space Used In Percent],
+	idx.Record_Count [Record Count],
+	Ghost_Record_Count [Ghost Record Count],
+	idx.Version_Ghost_Record_Count [Version Ghost Record Count],
+	idx.Min_Record_Size_In_Bytes [Min Record Size In Bytes],
+	idx.Max_Record_Size_In_Bytes [Max Record Size In Bytes],
+	idx.Avg_Record_Size_In_Bytes [Avg Record Size In Bytes],
+	Forwarded_Record_Count [Forwarded Record Count]
+FROM sys.dm_db_index_physical_stats(DB_ID(), OBJECT_ID('ClusteredIndexExample'), NULL, NULL, 'DETAILED') idx
+	INNER JOIN sys.tables t
+	ON idx.object_id = t.object_id
+	INNER JOIN sys.schemas sch
+	ON sch.schema_id = t.schema_id
+--WHERE idx.page_count > 100
+--	AND idx.avg_fragmentation_in_percent > 15
+--	AND idx.Index_ID > 0
+ORDER BY idx.avg_fragmentation_in_percent desc;
+
+
+
+WITH [Waits] AS
+    (SELECT
+        [wait_type],
+        [wait_time_ms] / 1000.0 AS [WaitS],
+        ([wait_time_ms] - [signal_wait_time_ms]) / 1000.0 AS [ResourceS],
+        [signal_wait_time_ms] / 1000.0 AS [SignalS],
+        [waiting_tasks_count] AS [WaitCount],
+        100.0 * [wait_time_ms] / SUM ([wait_time_ms]) OVER() AS [Percentage],
+        ROW_NUMBER() OVER(ORDER BY [wait_time_ms] DESC) AS [RowNum]
+    FROM sys.dm_os_wait_stats
+    WHERE [wait_type] NOT IN (
+        -- These wait types are almost 100% never a problem and so they are
+        -- filtered out to avoid them skewing the results. Click on the URL
+        -- for more information.
+        N'BROKER_EVENTHANDLER', -- https://www.sqlskills.com/help/waits/BROKER_EVENTHANDLER
+        N'BROKER_RECEIVE_WAITFOR', -- https://www.sqlskills.com/help/waits/BROKER_RECEIVE_WAITFOR
+        N'BROKER_TASK_STOP', -- https://www.sqlskills.com/help/waits/BROKER_TASK_STOP
+        N'BROKER_TO_FLUSH', -- https://www.sqlskills.com/help/waits/BROKER_TO_FLUSH
+        N'BROKER_TRANSMITTER', -- https://www.sqlskills.com/help/waits/BROKER_TRANSMITTER
+        N'CHECKPOINT_QUEUE', -- https://www.sqlskills.com/help/waits/CHECKPOINT_QUEUE
+        N'CHKPT', -- https://www.sqlskills.com/help/waits/CHKPT
+        N'CLR_AUTO_EVENT', -- https://www.sqlskills.com/help/waits/CLR_AUTO_EVENT
+        N'CLR_MANUAL_EVENT', -- https://www.sqlskills.com/help/waits/CLR_MANUAL_EVENT
+        N'CLR_SEMAPHORE', -- https://www.sqlskills.com/help/waits/CLR_SEMAPHORE
+ 
+        -- Maybe comment these four out if you have mirroring issues
+        N'DBMIRROR_DBM_EVENT', -- https://www.sqlskills.com/help/waits/DBMIRROR_DBM_EVENT
+        N'DBMIRROR_EVENTS_QUEUE', -- https://www.sqlskills.com/help/waits/DBMIRROR_EVENTS_QUEUE
+        N'DBMIRROR_WORKER_QUEUE', -- https://www.sqlskills.com/help/waits/DBMIRROR_WORKER_QUEUE
+        N'DBMIRRORING_CMD', -- https://www.sqlskills.com/help/waits/DBMIRRORING_CMD
+ 
+        N'DIRTY_PAGE_POLL', -- https://www.sqlskills.com/help/waits/DIRTY_PAGE_POLL
+        N'DISPATCHER_QUEUE_SEMAPHORE', -- https://www.sqlskills.com/help/waits/DISPATCHER_QUEUE_SEMAPHORE
+        N'EXECSYNC', -- https://www.sqlskills.com/help/waits/EXECSYNC
+        N'FSAGENT', -- https://www.sqlskills.com/help/waits/FSAGENT
+        N'FT_IFTS_SCHEDULER_IDLE_WAIT', -- https://www.sqlskills.com/help/waits/FT_IFTS_SCHEDULER_IDLE_WAIT
+        N'FT_IFTSHC_MUTEX', -- https://www.sqlskills.com/help/waits/FT_IFTSHC_MUTEX
+ 
+        -- Maybe comment these six out if you have AG issues
+        N'HADR_CLUSAPI_CALL', -- https://www.sqlskills.com/help/waits/HADR_CLUSAPI_CALL
+        N'HADR_FILESTREAM_IOMGR_IOCOMPLETION', -- https://www.sqlskills.com/help/waits/HADR_FILESTREAM_IOMGR_IOCOMPLETION
+        N'HADR_LOGCAPTURE_WAIT', -- https://www.sqlskills.com/help/waits/HADR_LOGCAPTURE_WAIT
+        N'HADR_NOTIFICATION_DEQUEUE', -- https://www.sqlskills.com/help/waits/HADR_NOTIFICATION_DEQUEUE
+        N'HADR_TIMER_TASK', -- https://www.sqlskills.com/help/waits/HADR_TIMER_TASK
+        N'HADR_WORK_QUEUE', -- https://www.sqlskills.com/help/waits/HADR_WORK_QUEUE
+ 
+        N'KSOURCE_WAKEUP', -- https://www.sqlskills.com/help/waits/KSOURCE_WAKEUP
+        N'LAZYWRITER_SLEEP', -- https://www.sqlskills.com/help/waits/LAZYWRITER_SLEEP
+        N'LOGMGR_QUEUE', -- https://www.sqlskills.com/help/waits/LOGMGR_QUEUE
+        N'MEMORY_ALLOCATION_EXT', -- https://www.sqlskills.com/help/waits/MEMORY_ALLOCATION_EXT
+        N'ONDEMAND_TASK_QUEUE', -- https://www.sqlskills.com/help/waits/ONDEMAND_TASK_QUEUE
+        N'PREEMPTIVE_XE_GETTARGETSTATE', -- https://www.sqlskills.com/help/waits/PREEMPTIVE_XE_GETTARGETSTATE
+        N'PWAIT_ALL_COMPONENTS_INITIALIZED', -- https://www.sqlskills.com/help/waits/PWAIT_ALL_COMPONENTS_INITIALIZED
+        N'PWAIT_DIRECTLOGCONSUMER_GETNEXT', -- https://www.sqlskills.com/help/waits/PWAIT_DIRECTLOGCONSUMER_GETNEXT
+        N'QDS_PERSIST_TASK_MAIN_LOOP_SLEEP', -- https://www.sqlskills.com/help/waits/QDS_PERSIST_TASK_MAIN_LOOP_SLEEP
+        N'QDS_ASYNC_QUEUE', -- https://www.sqlskills.com/help/waits/QDS_ASYNC_QUEUE
+        N'QDS_CLEANUP_STALE_QUERIES_TASK_MAIN_LOOP_SLEEP',
+            -- https://www.sqlskills.com/help/waits/QDS_CLEANUP_STALE_QUERIES_TASK_MAIN_LOOP_SLEEP
+        N'QDS_SHUTDOWN_QUEUE', -- https://www.sqlskills.com/help/waits/QDS_SHUTDOWN_QUEUE
+        N'REDO_THREAD_PENDING_WORK', -- https://www.sqlskills.com/help/waits/REDO_THREAD_PENDING_WORK
+        N'REQUEST_FOR_DEADLOCK_SEARCH', -- https://www.sqlskills.com/help/waits/REQUEST_FOR_DEADLOCK_SEARCH
+        N'RESOURCE_QUEUE', -- https://www.sqlskills.com/help/waits/RESOURCE_QUEUE
+        N'SERVER_IDLE_CHECK', -- https://www.sqlskills.com/help/waits/SERVER_IDLE_CHECK
+        N'SLEEP_BPOOL_FLUSH', -- https://www.sqlskills.com/help/waits/SLEEP_BPOOL_FLUSH
+        N'SLEEP_DBSTARTUP', -- https://www.sqlskills.com/help/waits/SLEEP_DBSTARTUP
+        N'SLEEP_DCOMSTARTUP', -- https://www.sqlskills.com/help/waits/SLEEP_DCOMSTARTUP
+        N'SLEEP_MASTERDBREADY', -- https://www.sqlskills.com/help/waits/SLEEP_MASTERDBREADY
+        N'SLEEP_MASTERMDREADY', -- https://www.sqlskills.com/help/waits/SLEEP_MASTERMDREADY
+        N'SLEEP_MASTERUPGRADED', -- https://www.sqlskills.com/help/waits/SLEEP_MASTERUPGRADED
+        N'SLEEP_MSDBSTARTUP', -- https://www.sqlskills.com/help/waits/SLEEP_MSDBSTARTUP
+        N'SLEEP_SYSTEMTASK', -- https://www.sqlskills.com/help/waits/SLEEP_SYSTEMTASK
+        N'SLEEP_TASK', -- https://www.sqlskills.com/help/waits/SLEEP_TASK
+        N'SLEEP_TEMPDBSTARTUP', -- https://www.sqlskills.com/help/waits/SLEEP_TEMPDBSTARTUP
+        N'SNI_HTTP_ACCEPT', -- https://www.sqlskills.com/help/waits/SNI_HTTP_ACCEPT
+        N'SP_SERVER_DIAGNOSTICS_SLEEP', -- https://www.sqlskills.com/help/waits/SP_SERVER_DIAGNOSTICS_SLEEP
+        N'SQLTRACE_BUFFER_FLUSH', -- https://www.sqlskills.com/help/waits/SQLTRACE_BUFFER_FLUSH
+        N'SQLTRACE_INCREMENTAL_FLUSH_SLEEP', -- https://www.sqlskills.com/help/waits/SQLTRACE_INCREMENTAL_FLUSH_SLEEP
+        N'SQLTRACE_WAIT_ENTRIES', -- https://www.sqlskills.com/help/waits/SQLTRACE_WAIT_ENTRIES
+        N'WAIT_FOR_RESULTS', -- https://www.sqlskills.com/help/waits/WAIT_FOR_RESULTS
+        N'WAITFOR', -- https://www.sqlskills.com/help/waits/WAITFOR
+        N'WAITFOR_TASKSHUTDOWN', -- https://www.sqlskills.com/help/waits/WAITFOR_TASKSHUTDOWN
+        N'WAIT_XTP_RECOVERY', -- https://www.sqlskills.com/help/waits/WAIT_XTP_RECOVERY
+        N'WAIT_XTP_HOST_WAIT', -- https://www.sqlskills.com/help/waits/WAIT_XTP_HOST_WAIT
+        N'WAIT_XTP_OFFLINE_CKPT_NEW_LOG', -- https://www.sqlskills.com/help/waits/WAIT_XTP_OFFLINE_CKPT_NEW_LOG
+        N'WAIT_XTP_CKPT_CLOSE', -- https://www.sqlskills.com/help/waits/WAIT_XTP_CKPT_CLOSE
+        N'XE_DISPATCHER_JOIN', -- https://www.sqlskills.com/help/waits/XE_DISPATCHER_JOIN
+        N'XE_DISPATCHER_WAIT', -- https://www.sqlskills.com/help/waits/XE_DISPATCHER_WAIT
+        N'XE_TIMER_EVENT' -- https://www.sqlskills.com/help/waits/XE_TIMER_EVENT
+        )
+    AND [waiting_tasks_count] > 0
+    )
+SELECT
+    MAX ([W1].[wait_type]) AS [WaitType],
+    CAST (MAX ([W1].[WaitS]) AS DECIMAL (16,2)) AS [Wait_S],
+    CAST (MAX ([W1].[ResourceS]) AS DECIMAL (16,2)) AS [Resource_S],
+    CAST (MAX ([W1].[SignalS]) AS DECIMAL (16,2)) AS [Signal_S],
+    MAX ([W1].[WaitCount]) AS [WaitCount],
+    CAST (MAX ([W1].[Percentage]) AS DECIMAL (5,2)) AS [Percentage],
+    CAST ((MAX ([W1].[WaitS]) / MAX ([W1].[WaitCount])) AS DECIMAL (16,4)) AS [AvgWait_S],
+    CAST ((MAX ([W1].[ResourceS]) / MAX ([W1].[WaitCount])) AS DECIMAL (16,4)) AS [AvgRes_S],
+    CAST ((MAX ([W1].[SignalS]) / MAX ([W1].[WaitCount])) AS DECIMAL (16,4)) AS [AvgSig_S],
+    CAST ('https://www.sqlskills.com/help/waits/' + MAX ([W1].[wait_type]) as XML) AS [Help/Info URL]
+FROM [Waits] AS [W1]
+INNER JOIN [Waits] AS [W2] ON [W2].[RowNum] <= [W1].[RowNum]
+GROUP BY [W1].[RowNum]
+HAVING SUM ([W2].[Percentage]) - MAX( [W1].[Percentage] ) < 95; -- percentage threshold
+GO
+
+
+DROP SEQUENCE ID
+DROP TABLE ClusteredIndexExample
