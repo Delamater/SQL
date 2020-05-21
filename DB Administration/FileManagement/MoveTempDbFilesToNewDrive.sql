c5a62f0e7844ef196aa77ac4fcac442c2358fea0
Bob Delamater <bob.delamater@sage.com>
2020-05-21 08:57:05 -0700

    Update MoveTempDbFilesToNewDrive.sql

--- DB Administration/FileManagement/MoveTempDbFilesToNewDrive.sql
+++ DB Administration/FileManagement/MoveTempDbFilesToNewDrive.sql
@@ -2,6 +2,10 @@
 SELECT 'ALTER DATABASE tempdb MODIFY FILE (NAME = [' + f.name + '],'
 	+ ' FILENAME = ''<FilePath, SYSNAME, D:\TempDB\>' + f.name
 	+ CASE WHEN f.type = 1 THEN '.ldf' ELSE '.mdf' END
-	+ ''');'
+	+ '''' 
+	+ CASE 
+		WHEN f.type = 1 THEN ', SIZE = <Log FileSize,INTEGER,10000>);'
+		ELSE ', SIZE = <FileSize,INTEGER,2000>);'
+	  END	
 FROM sys.master_files f
 WHERE f.database_id = DB_ID(N'tempdb');
