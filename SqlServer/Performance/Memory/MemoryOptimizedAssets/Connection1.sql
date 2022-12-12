5853a603fa0c2e8db3a315a65146daea49cad3c7
Bob Delamater <2991479+Delamater@users.noreply.github.com>
2022-12-12 12:49:31 -0800

    Memory Optimized testing

---
+++ SqlServer/Performance/Memory/MemoryOptimizedAssets/Connection1.sql
@@ -0,0 +1,27 @@
+-- Connection 1
+-- Dependency: CreateSampleData2 procedure from script CreateTable.sql
+USE TestDB
+GO
+EXEC CreateSampleData2 @IsOptimized = 1
+
+SELECT NAME, OBJECTPROPERTY(object_id,'TableIsMemoryOptimized') IsMemoryOptimized, * FROM sys.tables WHERE name IN('SampleData2', 'SampleData_SCHEMA_AND_DATA', 'SampleData_SCHEMA_ONLY')
+
+BEGIN TRY
+	BEGIN TRAN
+		-- Step: 1
+		UPDATE dbo.SampleData_SCHEMA_AND_DATA
+		SET StateID = 'ZZ'
+		WHERE ID = 1
+
+		-- Step: 3
+		UPDATE dbo.SampleData_SCHEMA_AND_DATA
+		SET StateID = 'ZZ'
+		WHERE ID = 2
+	--COMMIT
+END TRY
+BEGIN CATCH
+	SELECT ERROR_LINE, ERROR_MESSAGE, ERROR_NUMBER, ERROR_PROCEDURE, ERROR_SEVERITY, ERROR_STATE
+END CATCH
+
+
+ROLLBACK
---
+++ SqlServer/Performance/Memory/MemoryOptimizedAssets/Connection2.sql
@@ -0,0 +1,22 @@
+-- Connection 2
+USE TestDB
+GO
+SELECT NAME, OBJECTPROPERTY(object_id,'TableIsMemoryOptimized') IsMemoryOptimized FROM sys.tables WHERE name = 'SampleData'
+
+BEGIN TRY
+	BEGIN TRAN
+		-- Step: 2
+		UPDATE dbo.SampleData_SCHEMA_AND_DATA
+		SET StateID = 'BB'
+		WHERE ID = 1
+
+		-- Step: 4
+		UPDATE dbo.SampleData_SCHEMA_AND_DATA
+		SET StateID = 'BB'
+		WHERE ID = 2
+	--COMMIT
+END TRY
+BEGIN CATCH
+	SELECT ERROR_LINE, ERROR_MESSAGE, ERROR_NUMBER, ERROR_PROCEDURE, ERROR_SEVERITY, ERROR_STATE
+END CATCH
+ROLLBACK
